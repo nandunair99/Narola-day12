f87bdd4f147d56fd1c0e4622dae4e3d7b8552ffd
nandunair99 <71479564+nandunair99@users.noreply.github.com>
2022-01-21 10:18:13 +0530

    updated validation Operation and Registration

---
+++ Registration.java
@@ -0,0 +1,118 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Registration
+{
+	public static void main(String args[])
+	{
+		Person p1=new Person(1,"nandu", LocalDate.of(1999,07,21));
+		Person p2=new Person(2,"manu",LocalDate.of(2006,05,6));
+		Person p3=new Person(3,"maya",LocalDate.of(1973,10,3));
+		Person p4=new Person(4,"unni",LocalDate.of(1970,11,29));
+		
+		ArrayList<Person> list=new ArrayList<>();
+		list.add(p1);
+		list.add(p2);
+		list.add(p3);
+		list.add(p4);
+
+		ValidationOperations valOps=new ValidationOperations();
+		//method1
+		//valOps.getValidUsers(list,new CheckByAge());
+
+		//method2
+		/*
+		valOps.getValidUsers(list, new CheckPerson() {
+			@Override
+			public boolean test(Person p) {
+				if(p.getAge()>6 && p.getAge() <24)
+					return true;
+				else
+					return false;
+			}
+		});
+
+		 */
+		/*
+		valOps.getValidUsers(list, p-> {  //implementing test method of functional interface
+				if(p.getAge()>6 && p.getAge() <24)
+					return true;
+				else
+					return false;
+
+		});
+
+		 */
+//------------------------------------------------------------
+		//method1
+		/*
+		valOps.getValidUsers(list, new Predicate<Person>() {
+					@Override
+					public boolean test(Person p) {
+						if (p.getAge() > 6 && p.getAge() < 24)
+							return true;
+						else
+							return false;
+					}
+				},
+				new Consumer<Person>() {
+					@Override
+					public void accept(Person person) {
+						System.out.println(person.toString());
+					}
+				});
+		//method2
+		valOps.getValidUsers(list,  p-> {
+						if (p.getAge() > 6 && p.getAge() < 24)
+							return true;
+						else
+							return false;
+
+				},
+				p->System.out.println(p.toString())
+		);
+
+		 */
+		//-------------------------------------------
+		//method1
+		/*
+		valOps.getValidUsers(list, new Predicate<Person>() {
+					@Override
+					public boolean test(Person p) {
+						if (p.getAge() > 6 && p.getAge() < 24)
+							return true;
+						else
+							return false;
+					}
+				},
+				new Consumer<Person>() {
+					@Override
+					public void accept(Person person) {
+						System.out.println(person.name);
+					}
+				},
+				new Function<Person, String>() {
+					@Override
+					public String apply(Person person) {
+						return String.valueOf(person.getAge());
+					}
+				});
+
+		 */
+
+		//method2
+		valOps.getValidUsers(list,p-> {
+						if (p.getAge() > 6 && p.getAge() < 24)
+							return true;
+						else
+							return false;
+
+				},
+				person->System.out.println(person.name),
+				person->{return String.valueOf(person.getAge());});
+	}
+}
---
+++ ValidationOperations.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class ValidationOperations
+{
+	/*
+	public void getValidUsers(ArrayList<Person> list,CheckPerson tester)
+	{
+		for(Person p:list)
+		{
+			if(tester.test(p))
+				System.out.println(p.toString());
+		}
+	}*/
+	/*
+	public void getValidUsers(ArrayList<Person> list, Predicate<Person> tester)
+	{
+		for(Person p:list)
+		{
+			if(tester.test(p))
+				System.out.println(p.toString());
+		}
+	}
+
+	 */
+	/*
+	public void getValidUsers(ArrayList<Person> list, Predicate<Person> tester, Consumer<Person> accepter)
+	{
+		for(Person p:list)
+		{
+			if(tester.test(p))
+				accepter.accept(p);
+
+		}
+	}
+
+	 */
+
+	public void getValidUsers(ArrayList<Person> list, Predicate<Person> tester, Consumer<Person> accepter, Function<Person,String> applier)
+	{
+		for(Person p:list)
+		{
+			if(tester.test(p))
+			{
+				accepter.accept(p);
+				System.out.println(applier.apply(p));
+			}
+
+
+		}
+	}
+}
